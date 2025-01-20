61acb7f14fa7d023b1d5814f8def466ef36bd5fb
NickGacharich <Gach56@hotmail.com>
2025-01-19 22:30:32 -0500

    Update wsgi.py

--- my_town_square_bank/wsgi.py
+++ my_town_square_bank/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_town_square_bank.settings")
 
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
