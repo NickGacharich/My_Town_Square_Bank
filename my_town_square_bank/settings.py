a8d12edc8f465d6ec8f50467ced90d7081a95e94
NickGacharich <Gach56@hotmail.com>
2025-05-31 15:29:22 -0400

    Update settings.py

--- my_town_square_bank/settings.py
+++ my_town_square_bank/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = "/var/www/example.com/static/"
+STATIC_ROOT = "staticfiles"
 
 STATIC_URL = "static/"
 
