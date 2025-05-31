870ef29fadf5c582800f602f2659dea5c942f04c
NickGacharich <Gach56@hotmail.com>
2025-05-31 15:12:32 -0400

    Update settings.py

--- my_town_square_bank/settings.py
+++ my_town_square_bank/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +25,7 @@ SECRET_KEY = "django-insecure-2)72fwvs)60!s#jav#zs)4!#_v7oq(h%q_lfh!g-rzsr$2dlog
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'stark-crag-90296-c6b828b23ebc.herokuapp.com'] # Replace with your actual domain(s)
-
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -133,19 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "staticfiles/"
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'bank/templates'),
-    # Add to this list all the locations containing your static files 
-)
-
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+django_heroku.settings(locals())
